706beca12529e7009b2793d75b066a5c8808bbcc
Alice Kamenetsky <alicek@test1s-MacBook-Air.local>
2021-12-16 20:28:08 +0200

    Added delete+reminder toggle for tasks, class to display if reminder is on and created add task form comp

--- src/App.js
+++ src/App.js
@@ -7,19 +7,36 @@ function App() {
     {
         id: 1,
         text: 'hey1',
-        day: 'this is when this happened'
+        day: 'this is when this happened',
+        reminder: true
     },
     {
         id: 2,
         text: 'hey2',
-        day: 'this is when that happened'
+        day: 'this is when that happened',
+        reminder: false
     }
-])
+  ])
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? 
+        { ...task, reminder: !task.reminder } 
+      : 
+        task))
+  }
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? (
+          <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+        ) : (
+          'No tasks'
+        )}
     </div>
   );
 }
